732d0aa44ec6a3734d7294ea57586fe453a2fb97
omsfuk <omsfuk@163.com>
2017-07-19 15:46:24 +0800

    futher optimize

---
+++ server/src/main/java/cn/omsfuk/discount/controller/anonymous/MainController.java
@@ -0,0 +1,37 @@
+package cn.omsfuk.discount.controller.anonymous;
+
+import cn.omsfuk.discount.base.Result;
+import cn.omsfuk.discount.service.GoodsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by omsfuk on 2017/7/19.
+ */
+
+@RestController
+@RequestMapping("api/anonymous")
+public class MainController {
+
+    @Autowired
+    private GoodsService goodsService;
+
+    @RequestMapping(value = "comment/getComment", method = RequestMethod.GET)
+    public Result getComment(Integer id, Integer goodsID, Integer userID,
+                             @RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return goodsService.getComment(id, userID, goodsID, (page - 1) * rows, rows);
+    }
+
+    @RequestMapping(value = "goods/getGoods", method = RequestMethod.POST)
+    public Result getGoods(Integer id,
+                           @RequestParam(value = "page", defaultValue = "1") Integer page,
+                           @RequestParam(value = "rows", defaultValue = "10") Integer rows,
+                           String l1, String l2, String l3,
+                           Integer userID, Integer isValid) {
+        return goodsService.getGoods(id, userID, l1, l2, l3, isValid, (page - 1) * rows, rows);
+    }
+}
--- server/src/main/java/cn/omsfuk/discount/controller/normal/MainController.java
+++ server/src/main/java/cn/omsfuk/discount/controller/normal/MainController.java
@@ -1,12 +1,10 @@
-package cn.omsfuk.discount.controller;
+package cn.omsfuk.discount.controller.normal;
 
 import cn.omsfuk.discount.base.Result;
 import cn.omsfuk.discount.base.ResultCache;
-import cn.omsfuk.discount.dto.GoodsDto;
 import cn.omsfuk.discount.service.GoodsService;
+import cn.omsfuk.discount.service.UserService;
 import cn.omsfuk.discount.util.ObjectUtil;
-import cn.omsfuk.discount.util.SessionUtil;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,22 +12,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.util.UUID;
 
 /**
- * Created by omsfuk on 2017/7/17.
+ * Created by omsfuk on 2017/7/19.
  */
 
 @RestController
-@RequestMapping("api/goods")
-public class GoodsController {
+@RequestMapping("api/normal")
+public class MainController {
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private GoodsService goodsService;
 
-    @RequestMapping(value = "upload")
+    @RequestMapping(value = "favorite/addFavorite", method = RequestMethod.GET)
+    public Result addCollection(Integer goodsID) {
+        if (goodsID == null) {
+            return ResultCache.WRONG_PARAMETER_FORMAT;
+        }
+        return userService.addFavorite(goodsID);
+    }
+
+    @RequestMapping(value = "favorite/getFavorite", method = RequestMethod.GET)
+    public Result getCollection(@RequestParam(defaultValue = "1") Integer page,
+                                @RequestParam(defaultValue = "10") Integer rows) {
+        return userService.getFavorite((page - 1) * rows, rows);
+    }
+
+    @RequestMapping(value = "goods/upload")
     public Result upload(Integer type, String title, String description, String loc0, String loc1, String loc2,
                          Double longitude, Double latitude, Timestamp deadline, MultipartFile[] pic) {
         if (!ObjectUtil.notNull(type, loc0, loc1, loc2, longitude, latitude, deadline, pic)) {
@@ -38,27 +51,11 @@ public class GoodsController {
         return goodsService.uploadGoods(type, title, description, loc0, loc1, loc2, longitude, latitude, deadline, pic);
     }
 
-    @RequestMapping(value = "getGoods", method = RequestMethod.POST)
-    public Result getGoods(Integer id,
-                           @RequestParam(value = "page", defaultValue = "1") Integer page,
-                           @RequestParam(value = "rows", defaultValue = "10") Integer rows,
-                           String l1, String l2, String l3,
-                           Integer userID, Integer isValid) {
-        return goodsService.getGoods(id, userID, l1, l2, l3, isValid, (page - 1) * rows, rows);
-    }
-
-    @RequestMapping(value = "comment", method = RequestMethod.POST)
+    @RequestMapping(value = "comment/addComment", method = RequestMethod.POST)
     public Result comment(Integer goodsID, String content) {
         if (!ObjectUtil.notNull(goodsID, content)) {
             return ResultCache.WRONG_PARAMETER_FORMAT;
         }
         return goodsService.comment(goodsID, content);
-    }
-
-    @RequestMapping(value = "getComment", method = RequestMethod.GET)
-    public Result getComment(Integer id, Integer goodsID, Integer userID,
-                             @RequestParam(value = "page", defaultValue = "1") Integer page,
-                             @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
-        return goodsService.getComment(id, userID, goodsID, (page - 1) * rows, rows);
     }
 }
